713ef07457c7c04ecdc1c325a4347c6587923152
fabiolamin <fabio_lamin@hotmail.com>
2022-04-30 14:01:18 -0300

    Implements the basic player and camera movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,11 +8,15 @@ namespace BSTW.Player
     {
         private Vector2 _movement;
         private bool _canMove = false;
+        private bool isMoving = false;
 
         [SerializeField] private Rigidbody _playerRb;
+        [SerializeField] private Transform _thirdPersonCamera;
+
         [SerializeField] private float _waitingTimeToMove = 1f;
         [SerializeField] private float _movementSpeed = 5f;
         [SerializeField] private float _jumpingSpeed = 5f;
+        [SerializeField] private float _rotationSpeed = 5f;
         [SerializeField] private float _maxGroundDistance = 0.5f;
 
         private void Awake()
@@ -22,7 +26,23 @@ namespace BSTW.Player
 
         private void FixedUpdate()
         {
+            RotatePlayer();
             MovePlayer();
+        }
+
+        private void RotatePlayer()
+        {
+            if (isMoving)
+            {
+                var newRotation = Quaternion.Lerp(transform.rotation,
+                Quaternion.LookRotation(_thirdPersonCamera.transform.forward),
+                Time.deltaTime * _rotationSpeed);
+
+                newRotation.x = 0f;
+                newRotation.z = 0f;
+
+                transform.rotation = newRotation;
+            }
         }
 
         private IEnumerator WaitToMove(float duration)
@@ -35,18 +55,25 @@ namespace BSTW.Player
         private void MovePlayer()
         {
             if (_canMove)
-                _playerRb.velocity = new Vector3(_movement.x, _playerRb.velocity.y, _movement.y);
+            {
+                var forward = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
+                var right = new Vector3(transform.right.x, 0f, transform.right.z).normalized;
+                _playerRb.velocity = (forward * _movement.y) + (right * _movement.x) + Vector3.up * _playerRb.velocity.y;
+            }
         }
 
         public void OnMove(InputAction.CallbackContext value)
         {
             _movement = value.ReadValue<Vector2>() * _movementSpeed;
+            isMoving = value.performed;
         }
 
         public void OnJump(InputAction.CallbackContext value)
         {
             if (CanPlayerJump(value))
+            {
                 _playerRb.velocity += Vector3.up * _jumpingSpeed;
+            }
         }
 
         private bool CanPlayerJump(InputAction.CallbackContext value)
