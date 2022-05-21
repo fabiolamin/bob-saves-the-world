be0c8642be4f421ab74a137b16bed47c602de497
fabiolamin <fabio_lamin@hotmail.com>
2022-05-21 10:21:40 -0300

    Updates the camera FOV based if the player is aiming

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,15 +1,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 namespace BSTW.Player
 {
     public class PlayerShooting : MonoBehaviour
     {
+        private float _defaultCameraFOV;
+
+        [SerializeField] private CinemachineFreeLook _cinemachineCamera;
+        [SerializeField] private float _cameraFOVWhenAiming = 30f;
+        [SerializeField] private float _switchCameraFOVSpeed = 10f;
+
         public static bool IsAiming { get; private set; } = false;
+
+        private void Awake()
+        {
+            _defaultCameraFOV = _cinemachineCamera.m_Lens.FieldOfView;
+        }
+
+        private void Update()
+        {
+            UpdateCameraFOVOnAiming();
+        }
 
         public void OnAim(InputAction.CallbackContext value)
         {
             IsAiming = value.action.IsPressed() && PlayerFoot.IsOnTheGround;
+        }
+
+        private void UpdateCameraFOVOnAiming()
+        {
+            var targetFOV = IsAiming ? _cameraFOVWhenAiming : _defaultCameraFOV;
+
+            _cinemachineCamera.m_Lens.FieldOfView = Mathf.Lerp(
+                _cinemachineCamera.m_Lens.FieldOfView,
+                targetFOV,
+                _switchCameraFOVSpeed * Time.deltaTime);
         }
     }
 }
